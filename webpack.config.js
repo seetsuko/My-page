c11ba0e4d11bdd9d3690f20e142b42308f36e588
seetsuko <radgjms@gmail.com>
2023-04-13 15:01:38 +0900

    eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-var path = require('path');
-
+const path = require('path');
 module.exports = {
-    entry : './server/index.js',
-    target : 'node',
-    output : {
-        path : path.resolve(__dirname + '/dist'),
+    entry: './server/index.js',
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname + '/dist'),
         filename : 'main.js'
     },
     mode:'production'

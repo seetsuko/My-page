755d710f34694cbfbd2a0d4b8c227cd0f739c97a
seetsuko <radgjms@gmail.com>
2023-04-13 14:50:42 +0900

    station4 crear

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+    entry : './server/index.js',
+    target : 'node',
+    output : {
+        path : path.resolve(__dirname + '/dist'),
+        filename : 'main.js'
+    },
+    mode:'production'
+}
